424b03e0e260264af4fe2f8d9e1385daa07c1d02
AxelDutrieux <axel.dutrieux@edu.univ-fcomte.fr>
2024-04-11 14:21:40 +0200

    accordeon-2

--- src/pages/accordeon.vue
+++ src/pages/accordeon.vue
@@ -1,18 +1,21 @@
-<script setup lang="ts">
-//
+<script 
+setup lang="ts" 
+>
+import { ref } from 'vue'
+const sectionOpen = ref(1)
 </script>
 
 <template>
   <section>
-    <button class="text-xl">bouton 1</button>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam iusto facilis, unde, nostrum obcaecati explicabo enim iste corrupti, ad tempore odio velit iure est quidem voluptatem cumque molestias voluptas laudantium.</p>
+    <button class="text-xl" @pointerdown="sectionOpen=1">bouton 1</button>
+    <p v-show="sectionOpen===1">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam iusto facilis, unde, nostrum obcaecati explicabo enim iste corrupti, ad tempore odio velit iure est quidem voluptatem cumque molestias voluptas laudantium.</p>
   </section>
   <section>
-    <button class="text-xl">bouton 2</button>
-    <p>Mollitia dignissimos dolores laudantium cum excepturi odit velit commodi! Aperiam, enim tempore assumenda obcaecati repudiandae dolore perspiciatis laudantium amet praesentium est, minima officia eos vel at tenetur veritatis atque earum.</p>
+    <button class="text-xl" @pointerdown="sectionOpen=2">bouton 2</button>
+    <p v-show="sectionOpen===2">Mollitia dignissimos dolores laudantium cum excepturi odit velit commodi! Aperiam, enim tempore assumenda obcaecati repudiandae dolore perspiciatis laudantium amet praesentium est, minima officia eos vel at tenetur veritatis atque earum.</p>
   </section>
   <section>
-    <button class="text-xl">bouton 3</button>
-    <p>Accusantium quia odit recusandae obcaecati ipsa illo exercitationem dolorum corrupti facilis, deleniti tempore sequi dolore minima inventore ab voluptatem? Quod possimus dolore facere dignissimos! Enim ullam aliquam voluptatibus nihil eveniet?</p>
+    <button class="text-xl" @pointerdown="sectionOpen=3">bouton 3</button>
+    <p v-show="sectionOpen===3">Accusantium quia odit recusandae obcaecati ipsa illo exercitationem dolorum corrupti facilis, deleniti tempore sequi dolore minima inventore ab voluptatem? Quod possimus dolore facere dignissimos! Enim ullam aliquam voluptatibus nihil eveniet?</p>
   </section>
 </template>
