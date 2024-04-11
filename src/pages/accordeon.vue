c3a8e9d9b3a1f302f94b93dc43e608732f7433f8
AxelDutrieux <axel.dutrieux@edu.univ-fcomte.fr>
2024-04-11 14:14:19 +0200

    accordeon-1

---
+++ src/pages/accordeon.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+//
+</script>
+
+<template>
+  <section>
+    <button class="text-xl">bouton 1</button>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam iusto facilis, unde, nostrum obcaecati explicabo enim iste corrupti, ad tempore odio velit iure est quidem voluptatem cumque molestias voluptas laudantium.</p>
+  </section>
+  <section>
+    <button class="text-xl">bouton 2</button>
+    <p>Mollitia dignissimos dolores laudantium cum excepturi odit velit commodi! Aperiam, enim tempore assumenda obcaecati repudiandae dolore perspiciatis laudantium amet praesentium est, minima officia eos vel at tenetur veritatis atque earum.</p>
+  </section>
+  <section>
+    <button class="text-xl">bouton 3</button>
+    <p>Accusantium quia odit recusandae obcaecati ipsa illo exercitationem dolorum corrupti facilis, deleniti tempore sequi dolore minima inventore ab voluptatem? Quod possimus dolore facere dignissimos! Enim ullam aliquam voluptatibus nihil eveniet?</p>
+  </section>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,4 +4,7 @@
 
 <template>
   <h1 class="text-2xl">Bonjour monde !</h1>
+  <li>
+    <router-link to="/accordeon"> Accordeon </router-link>
+  </li>
 </template>
